e193659e020ba1ac66e8da348a6d4c93d9849e67
delmol <ben@delmol.uk>
2019-01-03 01:03:53 +0000

    .

--- lumi.py
+++ lumi.py
@@ -1,3 +1,10 @@
+from argparse import ArgumentParser
+
 '''
 start instance with command args
 '''
+
+parser = ArgumentParser()
+
+parser.add_argument("-n", "--node", dest="filename",
+                    help="write report to FILE", metavar="FILE")
